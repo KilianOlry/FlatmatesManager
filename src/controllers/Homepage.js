ffc3f2229015e248b1aade5f3aa08cb82ae5efa6
kilian <kilian.olry@gmail.com>
2024-05-13 16:46:49 +0200

    feat(index.scss / Homepage.js / login.scss / Login.js): add template form login

--- src/controllers/Homepage.js
+++ src/controllers/Homepage.js
@@ -26,6 +26,7 @@ const Homepage = class {
       ${viewNav()}
 
       <main>
+      
         ${viewBanner()}
         <h1 class="my-10 text-2xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Nous Vous</span> Proposons </h1>
         <div class='flex flex-wrap container mx-auto gap-16 justify-center my-12'>
