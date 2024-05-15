e637227382b99ddefee64152410cba87c74f442f
kilian <kilian.olry@gmail.com>
2024-05-15 21:51:58 +0200

    refactor(Sidebar.js): add sidebar into folder views

--- src/views/homepage/card.js
+++ src/views/homepage/card.js
@@ -4,7 +4,7 @@ export default (title) => (`
       <img class="w-full rounded-xl" src="https://img.freepik.com/vecteurs-libre/illustration-concept-petite-maison_114360-9087.jpg?t=st=1715605297~exp=1715608897~hmac=63558cb64e06406d159de9d88340f595dc16539276563cd633642784c4f0322c&w=1060" alt="Colors" />
     </div>
     <div class="my-4">
-      <button class="mt-4 text-xl capitalize w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">${title}</button>
+      <button class="test mt-4 text-xl capitalize w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">${title}</button>
     </div>
   </div>
 `);
