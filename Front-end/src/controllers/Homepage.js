cd5640f02120f271612cbde5dc589a94f48d1552
kilian <kilian.olry@gmail.com>
2024-06-05 10:44:42 +0200

    /FRONT-END/feat(Homepage / modal): send data form for create colocation

--- Front-end/src/controllers/Homepage.js
+++ Front-end/src/controllers/Homepage.js
@@ -1,3 +1,6 @@
+import toastr from 'toastr';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 import viewNav from '../views/global/nav';
 import viewBanner from '../views/global/banner';
 import viewFooter from '../views/global/footer';
@@ -26,8 +29,41 @@ const Homepage = class extends Auth {
     `;
   }
 
+  getDataForm() {
+    const formCreateHome = document.querySelector('.form-create-home');
+
+    formCreateHome.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const currentlyUser = JSON.parse(Cookies.get('Session'));
+      const formData = new FormData(formCreateHome);
+
+      const dataForm = {
+        name: formData.get('name'),
+        adress: formData.get('adress'),
+        userEmail: currentlyUser.email
+      };
+
+      this.sendData(dataForm);
+    });
+  }
+
+  sendData(dataForm) {
+    axios.post('http://localhost:50/home/create', dataForm, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(() => {
+        toastr.success('colocation crée');
+      })
+      .catch(() => {
+        toastr.error('erreur lors de la création de la colocation');
+      });
+  }
+
   async run() {
     this.el.innerHTML = await this.renderSkeleton();
+    this.getDataForm();
   }
 };
 
