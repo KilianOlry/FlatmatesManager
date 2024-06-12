09f5593fca708403d79cac2e3536871322995fa1
kilian <kilian.olry@gmail.com>
2024-06-12 22:19:47 +0200

    /FRONT-END/fixe(expenses / tasks) : fixe bug display dashboard when we don't have tasks or expenses

--- Front-end/src/views/admin/dashboard/expenses/expenses.js
+++ Front-end/src/views/admin/dashboard/expenses/expenses.js
@@ -1,9 +1,16 @@
 import expense from './expense';
 
-const renderComponent = (dataExpense) => `
-  <section class='flex gap-7 p-3'>
-    ${dataExpense.map((item) => expense(item)).join('')}
-  </section>
-  `;
+const renderComponent = (dataExpense) => {
+  const hasExpenses = Object.keys(dataExpense).length > 0;
+
+  const content = hasExpenses
+    ? `<section class='flex gap-7 p-3'>
+        ${dataExpense.map(expense).join('')}
+       </section>`
+
+    : "<h2 class='text-xl'>Vous n'avez pas de dépenses de prévu<h2>";
+
+  return content;
+};
 
 export default renderComponent;
--- Front-end/src/views/admin/dashboard/tasks/tasks.js
+++ Front-end/src/views/admin/dashboard/tasks/tasks.js
@@ -1,9 +1,16 @@
 import task from './task';
 
-const renderComponent = (dataTasks) => `
-  <section class='flex gap-7 p-3'>
-    ${dataTasks.map((item) => task(item)).join('')}
-  </section>
-  `;
+const renderComponent = (dataTasks) => {
+  const hasExpenses = Object.keys(dataTasks).length > 0;
+
+  const content = hasExpenses
+    ? `<section class='flex gap-7 p-3'>
+        ${dataTasks.map(task).join('')}
+       </section>`
+
+    : "<h2 class='text-xl'>Vous n'avez pas de dépenses de prévu<h2>";
+
+  return content;
+};
 
 export default renderComponent;
