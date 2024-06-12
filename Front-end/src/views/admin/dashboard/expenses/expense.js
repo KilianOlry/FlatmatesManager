9d7e3fb82a1454c14147d0653a0681e59947e104
kilian <kilian.olry@gmail.com>
2024-06-12 22:06:03 +0200

    /FRONT-END/style (expense / task) : add badge for date and status

--- Front-end/src/views/admin/dashboard/expenses/expense.js
+++ Front-end/src/views/admin/dashboard/expenses/expense.js
@@ -24,7 +24,7 @@ export default (item) => {
         <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z"></path>
       </svg>
     </div>
-    <p class="antialiased font-sans text-right text-sm leading-normal font-normal text-blue-gray-600">${date}</p>
+    <p class="absolute right-0 top-3 antialiased inline font-sans text-right text-sm blue-200 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">${date}</p>
   </div>
 
   <div class="mt-4 px-3">
@@ -33,12 +33,18 @@ export default (item) => {
   <div class="border-t border-blue-gray-50 p-4">
     <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">${description}</p>
   </div>
-    <div class='flex items-center justify-between px-4'>
+    <div class='flex items-center justify-between px-4 mb-2'>
     <form action="" class='change-status-expense'>
       <input type='hidden' value='${expense_id}' name='expense' class='test'>
-      <button type='submit'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M3 13.5a.5.5 0 0 1-.5-.5V3a.5.5 0 0 1 .5-.5h9.25a.75.75 0 0 0 0-1.5H3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9.75a.75.75 0 0 0-1.5 0V13a.5.5 0 0 1-.5.5zm12.78-8.82a.75.75 0 0 0-1.06-1.06L9.162 9.177L7.289 7.241a.75.75 0 1 0-1.078 1.043l2.403 2.484a.75.75 0 0 0 1.07.01z" clip-rule="evenodd"/></svg>
-      </button>
+      <span id="badge-dismiss-green" class="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-blue-500 blue-200 rounded dark:bg-green-900 dark:text-green-300">
+          Terminer
+          <button type="submit" class="inline-flex items-center p-1 ms-2 text-sm text-blue-200 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900" data-dismiss-target="#badge-dismiss-green" aria-label="Remove">
+            <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Remove badge</span>
+          </button>
+        </span>
     </form>
     </div>
 </div>
--- Front-end/src/views/admin/dashboard/tasks/task.js
+++ Front-end/src/views/admin/dashboard/tasks/task.js
@@ -20,7 +20,7 @@ export default (item) => {
     <div class="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-green-600 to-green-400 text-white shadow-green-500/40 shadow-lg absolute -top-2 -left-6 grid h-10 w-10 place-items-center">
       <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark-check"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2Z"/><path d="m9 10 2 2 4-4"/></svg>
     </div>
-    <p class="antialiased font-sans text-right text-sm leading-normal font-normal text-blue-gray-600">${date}</p>
+    <p class="absolute right-0 top-3 antialiased inline font-sans text-right text-sm bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">${date}</p>
   </div>
 
   <div class="mt-4 px-3">
@@ -29,14 +29,20 @@ export default (item) => {
   <div class="border-t border-blue-gray-50 p-4">
     <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">${description}</p>
   </div>
-    <div class='flex items-center justify-between px-4'>
-    <form action="" class='change-status-task'>
-      <input type='hidden' value='${id}' name='task' class='test'>
-      <button type='submit'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M3 13.5a.5.5 0 0 1-.5-.5V3a.5.5 0 0 1 .5-.5h9.25a.75.75 0 0 0 0-1.5H3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9.75a.75.75 0 0 0-1.5 0V13a.5.5 0 0 1-.5.5zm12.78-8.82a.75.75 0 0 0-1.06-1.06L9.162 9.177L7.289 7.241a.75.75 0 1 0-1.078 1.043l2.403 2.484a.75.75 0 0 0 1.07.01z" clip-rule="evenodd"/></svg>
-      </button>
-    </form>
-      <span class="priority-tag text-sm font-medium rounded">${priority === 'null' ? 'pas de description' : priority}</span>
+    <div class='flex items-center justify-between px-4 pb-2'>
+      <form action="" class='change-status-task'>
+        <input type='hidden' value='${id}' name='task' class='test'>
+        <span id="badge-dismiss-green" class="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-green-800 bg-green-100 rounded dark:bg-green-900 dark:text-green-300">
+          Terminer
+          <button type="submit" class="inline-flex items-center p-1 ms-2 text-sm text-green-400 bg-transparent rounded-sm hover:bg-green-200 hover:text-green-900 dark:hover:bg-green-800 dark:hover:text-green-300" data-dismiss-target="#badge-dismiss-green" aria-label="Remove">
+            <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Remove badge</span>
+          </button>
+        </span>
+      </form>
+      <span class="priority-tag text-sm font-medium">${priority === 'null' ? 'pas de description' : priority}</span>
     </div>
 </div>
     `;
