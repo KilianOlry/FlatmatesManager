c728dabd7d09ce315eba20b9d6ff1f5cf2b7e02e
kilian <kilian.olry@gmail.com>
2024-07-04 23:19:25 +0200

    /DOC/doc(README, flatmate.sql) : update sql file and readme

--- README.md
+++ README.md
@@ -15,36 +15,88 @@
   <img alt="Repository size" src="https://img.shields.io/github/repo-size/KilianOlry/FlatmatesManager?color=56BEB8">
 </p>
 
-## :checkered_flag: Identifiants ##
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="https://github.com/Raxuis" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About
+
+FlatMatesManager is a App who simplify the life in FlatMates.
+We can manage Expenses | Tasks | Messages ...
+
+## :sparkles: Features
+
+- Manage Tasks
+- Manage Expenses
+- Manage Messages
+- Manage User
+- join Flatmates
+- show event on calendar view
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [JavaScript](https://developer.mozilla.org/fr/)
+- [TailwindCSS](https://tailwindcss.com/)
+- [PHP](https://www.php.net/)
+- [MySQL](https://www.mysql.com/fr/)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) | [Node](https://nodejs.org/en/) | [Composer](https://getcomposer.org/) installed .
+
+## :checkered_flag: Starting
 
 ```bash
-# Pour le FRONT-END
-cd app-colo/Front-end
-npm i
-npm start ou npm run start
-
-# Pour le BACK-END
-composer install
-composer dump-autolad
-
-# Pour changer les informations de connexion à la bdd par les votres
-cd BACK-END/src/models/SqlConnect.php
+# Clone this project
+$ git clone https://github.com/KilianOlry/FlatmatesManager
 ```
 
-## :checkered_flag: Identifiants ##
+### Frontend Installation
 
 ```bash
-# IDENTIFIANTS ADMIN AVEC COLOCATION
-email : admin@admin.com
-password: Pa$$w0rd!
+# Front-End
+$ cd front-End
 
-# IDENTIFIANTS USER SANS COLOCATION
-email: pyvihem@mailinator.com
-password: Pa$$w0rd!
+# Install dependencies
+$ npm i | npm install
 
-# TOKEN POUR REJOINDRE UNE COLOCATION
-password : home5
+# Run the project
+$ npm run start
+
+# The server will initialize in the <http://127.0.0.1:9090>
 ```
+
+### Backend Installation
+
+```bash
+# Back-End
+$ cd backend
+
+# Install dependencies
+$ composer install
+
+# Don't forget to
+$ composer dump-autoload
+
+# Start your Nginx server and PHP server
+
+# For Windows
+$ sudo systemctl start php@8.1 && sudo systemctl start nginx
+
+# Start your MAMP / XAMP / LAMP server
+
+# Get the DB file at /app-colo
+```
+
 
 Made with :heart: by <a href="https://github.com/KilianOlry" target="_blank">Kilian</a>
 
--- flatmates.sql
+++ flatmates.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : dim. 16 juin 2024 à 22:23
--- Version du serveur : 8.0.36-0ubuntu0.22.04.1
+-- Généré le : jeu. 04 juil. 2024 à 23:18
+-- Version du serveur : 8.0.37-0ubuntu0.22.04.3
 -- Version de PHP : 8.1.2-1ubuntu2.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -77,7 +77,7 @@ CREATE TABLE `expenses` (
   `description` text COLLATE utf8mb4_general_ci,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_limit` datetime NOT NULL,
-  `status` enum('Payé','Impayé') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Impayé',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
   `category_id` int NOT NULL,
   `user_id` int NOT NULL,
   `home_id` int NOT NULL
@@ -88,8 +88,23 @@ CREATE TABLE `expenses` (
 --
 
 INSERT INTO `expenses` (`id`, `price`, `description`, `created_at`, `date_limit`, `status`, `category_id`, `user_id`, `home_id`) VALUES
-(16, 45, 'Plombier', '2024-06-15 22:00:00', '2024-06-19 12:54:00', 'Impayé', 2, 9, 5),
-(17, 127, 'Loyer', '2024-06-15 22:00:00', '2024-06-21 15:54:00', 'Impayé', 1, 9, 5);
+(19, 427, 'Ut non odit doloremq', '2024-06-18 22:00:00', '1975-06-05 09:53:00', 1, 1, 9, 5),
+(20, 527, 'Ut qui voluptatem f', '2024-06-18 22:00:00', '2005-03-26 03:24:00', 1, 1, 9, 5),
+(21, 15, 'Eiusmod Nam veniam ', '2024-06-18 22:00:00', '1980-09-01 21:00:00', 1, 3, 9, 5),
+(22, 33, 'Sunt quidem consequ', '2024-06-18 22:00:00', '1990-08-15 09:51:00', 1, 4, 9, 5),
+(23, 638, 'In delectus veniam', '2024-06-19 22:00:00', '1982-12-14 05:01:00', 1, 1, 9, 5),
+(24, 373, 'Do unde repudiandae ', '2024-06-20 22:00:00', '2020-05-06 03:29:00', 1, 3, 9, 5),
+(25, 69, 'Debitis harum dolore', '2024-06-27 22:00:00', '2023-04-12 00:01:00', 1, 1, 9, 5),
+(26, 149, 'Debitis harum dolore', '2024-06-27 22:00:00', '2023-04-12 00:01:00', 1, 1, 9, 5),
+(27, 484, 'Ut nisi perspiciatis', '2024-06-30 22:00:00', '2008-11-07 03:56:00', 1, 1, 9, 5),
+(28, 607, 'Consequatur nisi sit', '2024-06-30 22:00:00', '2019-07-20 10:56:00', 0, 4, 21, 5),
+(29, 840, 'Itaque dolore enim p', '2024-06-30 22:00:00', '1982-08-09 22:15:00', 1, 2, 9, 5),
+(30, 965, 'Qui sunt nostrum nu', '2024-07-03 22:00:00', '1971-04-19 05:43:00', 1, 2, 9, 5),
+(31, 45, '', '2024-07-03 22:00:00', '2010-04-06 10:21:00', 0, 2, 10, 5),
+(32, 527, 'Ut id ipsa in alias', '2024-07-03 22:00:00', '2008-10-11 16:32:00', 0, 3, 9, 5),
+(33, 520, 'Quia voluptas dolore', '2024-07-03 22:00:00', '2024-07-05 09:20:00', 0, 4, 10, 5),
+(34, 279, 'Excepteur qui labori', '2024-07-03 22:00:00', '2024-07-05 01:58:00', 0, 4, 9, 5),
+(35, 39, 'Ut consectetur repre', '2024-07-03 22:00:00', '2024-07-05 10:33:00', 0, 3, 9, 5);
 
 -- --------------------------------------------------------
 
@@ -113,7 +128,11 @@ INSERT INTO `homes` (`id`, `adress`, `name`, `token`) VALUES
 (2, '9 rue Henri Lavedan', 'Playstation', '39207275652048656e7269204c61766564616e'),
 (3, 'Nisi mollit eos aliq', 'Chandler Pate', '4e697369206d6f6c6c697420656f7320616c6971'),
 (4, 'Saepe molestiae debi', 'Francesca Austin', '5361657065206d6f6c6573746961652064656269'),
-(5, 'Omnis animi ad numq', 'Rhea French', 'home5');
+(5, 'Omnis animi ad numq', 'Rhea French', 'home5'),
+(11, 'Ut aperiam dolorem v', 'Amal Ware', '&8DndG'),
+(12, 'Esse porro nemo enim', 'Knox Bell', '2z)QGt'),
+(13, 'Esse porro nemo enim', 'Knox Bell', 'QOlPXh'),
+(14, '9 rue Henri Lavedan', 'Playstation', 'LaaF70');
 
 -- --------------------------------------------------------
 
@@ -135,7 +154,18 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `title`, `content`, `created_at`, `user_id`, `home_id`) VALUES
-(3, 'Conference', 'Conference Green IT', '2024-06-16 14:56:15', 10, 5);
+(3, 'Conference', 'Conference Green IT', '2024-06-16 14:56:15', 10, 5),
+(4, 'Cillum dolore quaera', 'Ut et qui neque veli', '2024-06-20 13:16:39', 9, 5),
+(16, 'Consequatur dolorib', 'Eiusmod sint et quia', '2024-06-28 12:44:06', 9, 5),
+(17, 'Voluptas ab odio ver', 'Neque mollitia quod ', '2024-06-30 09:47:01', 9, 5),
+(18, 'Voluptate cumque dol', 'Sit et numquam Nam ', '2024-06-30 15:50:48', 9, 5),
+(19, 'Enim voluptas est de', 'At voluptas laborum ', '2024-06-30 15:51:47', 9, 5),
+(20, 'Enim voluptas est de', 'At voluptas laborum ', '2024-06-30 15:52:33', 9, 5),
+(21, 'Ut qui sed expedita ', 'Repellendus Volupta', '2024-06-30 15:53:50', 9, 5),
+(22, 'Sunt similique quae', 'Itaque eum animi nu', '2024-06-30 15:54:41', 9, 5),
+(23, 'Ullam aliquip perspi', 'Aut sed quia qui sit', '2024-06-30 15:55:04', 9, 5),
+(24, 'Iure labore perspici', 'Tempore voluptatem', '2024-06-30 15:55:19', 9, 5),
+(25, 'Sunt doloribus tempo', 'Sit dolor possimus', '2024-07-04 14:57:45', 9, 5);
 
 -- --------------------------------------------------------
 
@@ -160,8 +190,24 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `description`, `created_at`, `date_limit`, `status`, `priority`, `category_id`, `user_id`, `home_id`) VALUES
-(43, 'Sel, Beurre, Coca', '2024-06-16 14:54:16', '2024-06-22 16:53:00', 'Pas Fait', 'PAS PRIORITAIRE', 2, 9, 5),
-(44, 'Surtout la jaune', '2024-06-16 14:54:42', '2024-06-20 18:53:00', 'Pas Fait', 'PAS PRIORITAIRE', 4, 9, 5);
+(45, 'Possimus rem conseq', '2024-06-20 12:28:01', '2001-07-17 23:49:00', 'Terminé', 'URGENT', 4, 9, 5),
+(46, 'Numquam pariatur Qu', '2024-06-20 13:23:20', '2024-06-20 10:38:00', 'Terminé', 'URGENT', 2, 10, 5),
+(47, 'Consectetur do commo', '2024-06-20 13:24:02', '2024-06-20 06:57:00', 'Terminé', 'PRIORITAIRE', 3, 9, 5),
+(55, 'Quo esse soluta alia', '2024-06-20 19:13:31', '2023-10-19 19:22:00', 'Terminé', 'PRIORITAIRE', 2, 9, 5),
+(56, 'Ea ut enim sint qua', '2024-06-21 07:27:16', '1986-05-13 00:17:00', 'Terminé', 'PAS PRIORITAIRE', 2, 9, 5),
+(57, 'Reiciendis ad aliqui', '2024-06-21 11:45:07', '1981-11-17 12:52:00', 'Terminé', 'URGENT', 2, 9, 5),
+(58, 'Fugiat molestiae err', '2024-06-28 12:40:52', '2012-06-22 17:34:00', 'Terminé', 'URGENT', 1, 9, 5),
+(59, 'A veritatis iure ips', '2024-07-01 13:40:51', '2022-06-12 05:37:00', 'Terminé', 'PAS PRIORITAIRE', 1, 9, 5),
+(60, 'Quibusdam exercitati', '2024-07-01 13:43:35', '1979-04-05 09:41:00', 'Terminé', 'PRIORITAIRE', 4, 9, 5),
+(61, 'Enim quibusdam quo l', '2024-07-01 13:51:43', '1999-06-28 00:13:00', 'Terminé', 'PAS PRIORITAIRE', 1, 9, 5),
+(62, 'Enim quibusdam quo l', '2024-07-01 13:51:44', '1999-06-28 00:13:00', 'Terminé', 'PAS PRIORITAIRE', 1, 9, 5),
+(63, 'Enim quibusdam quo l', '2024-07-01 13:51:44', '1999-06-28 00:13:00', 'Terminé', 'PAS PRIORITAIRE', 1, 9, 5),
+(64, 'Enim quibusdam quo l', '2024-07-01 13:51:45', '1999-06-28 00:13:00', 'Terminé', 'PAS PRIORITAIRE', 1, 9, 5),
+(65, 'Enim quibusdam quo l', '2024-07-01 13:51:45', '1999-06-28 00:13:00', 'Terminé', 'PAS PRIORITAIRE', 1, 9, 5),
+(66, 'Enim quibusdam quo l', '2024-07-01 13:51:45', '1999-06-28 00:13:00', 'Pas Fait', 'PAS PRIORITAIRE', 1, 9, 5),
+(67, 'Similique non eius v', '2024-07-01 20:52:54', '2024-07-02 18:30:00', 'Pas Fait', 'PRIORITAIRE', 2, 9, 5),
+(68, 'yoyoyoyoy', '2024-07-04 14:31:55', '2018-09-18 14:39:00', 'Terminé', 'URGENT', 1, 9, 5),
+(69, 'Ut sit id quis conse', '2024-07-04 14:33:40', '1975-07-15 05:43:00', 'Pas Fait', 'PAS PRIORITAIRE', 2, 21, 5);
 
 -- --------------------------------------------------------
 
@@ -176,7 +222,7 @@ CREATE TABLE `users` (
   `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `token` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `role` enum('ADMIN','USER') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'USER',
+  `role` enum('ADMIN','USER','SUPERADMIN') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'USER',
   `home_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -186,9 +232,14 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `token`, `role`, `home_id`) VALUES
 (8, 'Jeanette', 'Cooke', 'rado@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$dFBWOU9ZeEJkeEVqWEwvNg$whL3sBlr5gtE2NRxbJgKhncCeGuHGtyooprQ78NqeL4', '7261646f406d61696c696e61746f722e636f6d', 'USER', 1),
-(9, 'Laetitia', 'admin', 'admin@admin.com', '$argon2id$v=19$m=65536,t=4,p=1$dFBWOU9ZeEJkeEVqWEwvNg$whL3sBlr5gtE2NRxbJgKhncCeGuHGtyooprQ78NqeL4', 'admin', 'ADMIN', 5),
+(9, 'Laetitia', 'Laetitia', 'laetitia.laetitia@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$dFBWOU9ZeEJkeEVqWEwvNg$whL3sBlr5gtE2NRxbJgKhncCeGuHGtyooprQ78NqeL4', 'admin', 'ADMIN', 5),
 (10, 'Kilian', 'test', 'kilian.olry@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$dFBWOU9ZeEJkeEVqWEwvNg$whL3sBlr5gtE2NRxbJgKhncCeGuHGtyooprQ78NqeL4', 'srerzer', 'USER', 5),
-(21, 'Cedric', 'Ayala', 'pyvihem@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$cUxIQTdZN2JkRW5mODN5Yw$MdECX5gNVM08xtkmyYlKUj40sNkc4FTPKUgSrngS7ac', '7079766968656d406d61696c696e61746f722e636f6d', 'USER', NULL);
+(21, 'Cedric', 'Ayala', 'pyvihem@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$cUxIQTdZN2JkRW5mODN5Yw$MdECX5gNVM08xtkmyYlKUj40sNkc4FTPKUgSrngS7ac', '7079766968656d406d61696c696e61746f722e636f6d', 'USER', 5),
+(23, 'Cedrac', 'Simon', 'lipigip@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$UlppZUkvOW5PeTJXN1UzNQ$HxAT1csE/itLZfrl5gAui6W9nbA9Z9KXCWY+Wbh09Is', '6c697069676970406d61696c696e61746f722e636f6d', 'USER', 14),
+(24, 'Brianna', 'Norton', 'juqiwig@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$L1UxU0JtNFpIZ25JUGQwRA$vlQxiAZaZwJUUmFfGfu0IHbkM8eAaH4473xWRwZwZ7Y', '6a757169776967406d61696c696e61746f722e636f6d', 'USER', NULL),
+(25, 'Linda', 'Keller', 'fira@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$Nk9qZXhqdXh0b3NHZXkyMw$yirg7N5mL69OSeb4gSP7A6TE87pdwvAnAgp1Tc2Lp64', '66697261406d61696c696e61746f722e636f6d', 'USER', NULL),
+(26, 'Veronica', 'Duke', 'qapypokovo@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$QWJyMjB2L3hjR3FBV0w3Qg$CYJeTeX8Cttp/+nWgutJmLLmPSOq/+spXmj8btvu1bw', '71617079706f6b6f766f406d61696c696e61746f722e636f6d', 'USER', NULL),
+(27, 'Athena', 'Robles', 'lywaxysy@mailinator.com', '$argon2id$v=19$m=65536,t=4,p=1$RExtZFZMSzcuL0NYR3FOSw$3tZ1IOHF8JVcwqfsircPhCifSg3eE/tqHPGMO7ccjJs', '6c79776178797379406d61696c696e61746f722e636f6d', 'USER', NULL);
 
 --
 -- Index pour les tables déchargées
@@ -266,31 +317,31 @@ ALTER TABLE `categorys_task`
 -- AUTO_INCREMENT pour la table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT pour la table `homes`
 --
 ALTER TABLE `homes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pour la table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Contraintes pour les tables déchargées
